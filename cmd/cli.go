c716245980ef7710023be79a85c697f07c59eb73
soeren <2378192-soerenschneider@users.noreply.gitlab.com>
2021-09-28 15:04:19 +0200

    feat: Print version info

--- cmd/cli.go
+++ cmd/cli.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"acmevault/internal"
 	"flag"
+	"fmt"
 	"github.com/rs/zerolog/log"
 	"os"
 	"os/user"
@@ -12,14 +14,21 @@ import (
 const (
 	envConfFile = "ACME_VAULT_CONFIG_FILE"
 	cliConfFile = "conf"
+	cliVersion = "version"
 )
 
 func ParseCliFlags() (configFile string) {
 	flag.StringVar(&configFile, cliConfFile, os.Getenv(envConfFile), "path to the config file")
+	version := flag.Bool(cliVersion, false, "Print version and exit")
 	flag.Parse()
 
+	if *version {
+		fmt.Printf("%s (revision %s)", internal.BuildVersion, internal.CommitHash)
+		os.Exit(0)
+	}
+
 	if len(configFile) == 0 {
-		log.Fatal().Msgf("No configfile specified, use flag '-%s' or env var '%s'", cliConfFile, envConfFile)
+		log.Fatal().Msgf("No config file specified, use flag '-%s' or env var '%s'", cliConfFile, envConfFile)
 	}
 
 	if strings.HasPrefix(configFile, "~/") {
