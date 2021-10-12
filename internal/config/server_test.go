2e3ecb7546b67475148bdebefae24a29b6639c3b
soeren <2378192-soerenschneider@users.noreply.gitlab.com>
2021-10-12 21:13:23 +0200

    test: update tests

---
+++ internal/config/server_test.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestAcmeVaultServerConfigFromFile(t *testing.T) {
+	tests := []struct {
+		name    string
+		path    string
+		want    AcmeVaultServerConfig
+		wantErr bool
+	}{
+		{
+			name: "example server config",
+			path: "../../contrib/server.json",
+			want: AcmeVaultServerConfig{
+				VaultConfig: VaultConfig{
+					VaultToken:            "",
+					VaultAddr:             "https://vault:8200",
+					SecretId:              "secretId",
+					RoleId:                "roleId",
+					TokenIncreaseSeconds:  0,
+					TokenIncreaseInterval: 0,
+					PathPrefix:            "preprod",
+				},
+				AcmeConfig: AcmeConfig{
+					Email:           "my@email.tld",
+					AcmeUrl:         letsEncryptUrl,
+					AcmeDnsProvider: "",
+				},
+				IntervalSeconds: 43200,
+				Domains:         []string{"domain1.tld", "domain2.tld"},
+				MetricsAddr:     "127.0.0.1:9112",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := AcmeVaultServerConfigFromFile(tt.path)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("AcmeVaultServerConfigFromFile() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("AcmeVaultServerConfigFromFile() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
