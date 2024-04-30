6afbc1303ba2f61ef111efdc7fb6b6c0e0281c30
soeren <56670304+soerenschneider@users.noreply.github.com>
2024-04-30 21:12:19 +0200

    test: fix non-existing method

--- internal/config/server_test.go
+++ internal/config/server_test.go
@@ -77,7 +77,7 @@ func TestAcmeVaultServerConfigFromFile(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := Read(tt.path)
+			got, err := read(tt.path)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Read() error = %v, wantErr %v", err, tt.wantErr)
 				return
